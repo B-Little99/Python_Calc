dbd57a0befcd89d37c5a484e461c0a9458c5f1b9
Brandon Little <brandonlittle1999@hotmail.com>
2020-08-05 12:18:57 +0100

    Added files and created basic calculation functions.

---
+++ pythonCalc.py
@@ -0,0 +1,20 @@
+print('Welcome to SimpleCalc! \n \n This is a simplistic calculator. We only handle addition, multiplication, subtraction and division. Please enter your calculation: ')
+
+calculation = input()
+
+def addition(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+def multiplication(a, b):
+    result = a * b
+    return result
+
+def division(a, b):
+    result = a / b
+    return result
+
