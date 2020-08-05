690265f3d67454c03a8fa927126cdee0540f37ec
Brandon Little <brandonlittle1999@hotmail.com>
2020-08-05 14:09:02 +0100

    Added loop for calculations.

--- pythonCalc.py
+++ pythonCalc.py
@@ -1,20 +1,52 @@
-print('Welcome to SimpleCalc! \n \n This is a simplistic calculator. We only handle addition, multiplication, subtraction and division. Please enter your calculation: ')
-
-calculation = input()
-
 def addition(a, b):
+    a = int(a)
+    b = int(b)
     result = a + b
-    return result
+    print(result)
 
 def subtraction(a, b):
+    a = int(a)
+    b = int(b)
     result = a - b
-    return result
+    print(result)
 
 def multiplication(a, b):
+    a = int(a)
+    b = int(b)
     result = a * b
-    return result
+    print(result)
 
 def division(a, b):
+    a = int(a)
+    b = int(b)
     result = a / b
-    return result
+    print(result)
 
+print('\n \nWelcome to SimpleCalc! \n \nThis is a simplistic calculator. We only handle addition, multiplication, subtraction and division of two numbers only.')
+
+loop = True
+
+while loop == True:
+
+    print('Please enter your calculation or enter "done" if you wish to exit: ')
+
+    calculation = input()
+
+    if calculation == 'done':
+        print('Thanks for using SimpleCalc! Have a good day.')
+        loop = False
+    else: 
+        calculationList = calculation.split()
+
+        num1 = calculationList[0]
+        calcSymbol = calculationList[1]
+        num2 = calculationList[2]
+    
+        if calcSymbol == '+':
+            addition(num1, num2)
+        elif calcSymbol == '-':
+            subtraction(num1, num2)
+        elif calcSymbol == '*':
+            multiplication(num1, num2)
+        else:
+            division(num1, num2)
